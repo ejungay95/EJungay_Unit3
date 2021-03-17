a29d1c4501346cab39ba39a5dedecca089da50db
ejungay95 <elijah.jungay@gmail.com>
2021-03-17 13:56:08 -0700

    Finished Unit3: LessonA

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float jumpForce = 10f;
+  public float gravityModifier;
+
+  private bool onGround = true;
+  private Rigidbody rb;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    Physics.gravity *= gravityModifier;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+    if(spaceDown && onGround)
+    {
+      // player jumps
+      rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+      onGround = false;
+    }
+  }
+
+  private void OnCollisionEnter(Collision collision) {
+    onGround = true;
+  }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+  public GameObject prefab;
+  private Vector3 spawnPos = new Vector3(15f, 0, 0);
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    InvokeRepeating("SpawnObstacle", 1.5f, 1.5f);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+
+  void SpawnObstacle()
+  {
+    Instantiate(prefab, spawnPos, prefab.transform.rotation);
+  }
+}
